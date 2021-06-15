1e18ee70dd1a061729f7f42cc2859a84c82c10c6
JvWelzenis <83783857+JvWelzenis@users.noreply.github.com>
2021-06-15 10:02:57 +0200

    Add files via upload

---
+++ script.js
@@ -0,0 +1,36 @@
+/*function declaration 
+
+function squaredNumbers(number1, number2){
+    const square1 = number1 * number1;
+    const square2= number2 * number2;
+    const total = square1 + square2;
+    const totalSquare = total * total;
+    return totalSquare;
+}
+
+console.log(squaredNumbers(4,6));*/
+
+/*function expression
+
+const squaredNumbers1 = function(number3, number4){
+    const square3 = number3 * number3;
+    const square4 = number4 * number4;
+    const total = square3 + square4;
+    const totalSquare = total * total;
+    return totalSquare;
+};
+
+console.log(squaredNumbers1(4,6)); */
+
+//arrow function
+
+const squarednumbers2 = (number5, number6) => {
+    const square5 = number5 * number5;
+    const square6 = number6 * number6;
+    const total = square5 + square6;
+    const totalSquare = total * total;
+    return totalSquare;
+};
+
+console.log(squarednumbers2(4,6));
+
